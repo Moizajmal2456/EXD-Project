7b3b89d5eaaf359e25d4f490d8b6b75cd62059fe
Moizajmal2456 <moizajmal2456@gmail.com>
2023-05-22 22:22:03 +0500

    Set Hero section

---
+++ src/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,18 @@
+import style from "./styles.module.scss";
+import HeroImage from "../../Images/herosection.jpg";
+
+export const HeroSection = () => {
+return(
+    <div className={style.HeroSection_Erapper}> 
+     <div className={style.Left_Section}>
+      <h2>Catch your own style and look</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolor ibusperferendis suscipit
+         fugit voluptatum sequi et ratione provident inventore fugiat. Impedit, adipisci mollitia harum fuga voluptates aliquid eaque consectetur repudiandae?</p>
+         <button>Checkout</button>
+     </div>
+     <div className={style.Right_Section}>
+        <img  src={HeroImage} alt="Hero section image" />
+     </div>
+    </div>
+);
+};
