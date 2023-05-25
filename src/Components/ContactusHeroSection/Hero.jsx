bda4180989ce8d23be6a82846dfce6b4b82f7348
Moizajmal2456 <moizajmal2456@gmail.com>
2023-05-25 23:00:45 +0500

    Add contact us page

---
+++ src/Components/ContactusHeroSection/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Contactus from "../../Images/Contact Us.jpeg";
+import style from "./styles.module.scss";
+
+export const ContactPage = () => {
+  return (
+    <>
+    <div className={style.Contact_Wrapper}>
+      <div className={style.Left_Section}>
+        <h2>Contact form</h2>
+        <h4>Drop Us a Line</h4>
+        <p>Your email address will not be published. Required fields are marked *</p>
+        <div className={style.name}>
+        <input className={style.Email} type="text" placeholder='Enter Name'/>
+        <input className={style.Email} type="email" placeholder='Enter Email'/>
+        </div>
+        <div className={style.tel}>
+        <input className={style.Email} type="tel" placeholder='Enter Mobile Number'/>
+        <input className={style.Email} type="text" placeholder='Subject'/>
+        </div>
+            <textarea className={style.Email} type="text" placeholder='Message' rows="10"/>
+      </div>
+      <div className={style.Right_Section}>
+        <img src={Contactus} alt="" />
+      </div>
+    </div>
+    <div className={style.Button}>
+     <button>Submit</button>
+    </div>
+     </>
+  );
+};
+
--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,10 +1,11 @@
+import { ContactPage } from "../Components/ContactusHeroSection/Hero";
 import { Footer } from "../Components/Footer/Footer";
 import { Navbar } from "../Components/Navbar/Navbar";
-
 
 export const ContactUsPage = () => {
 return(<>
 <Navbar/>
+<ContactPage/>
 <Footer/>
 </>
 );
