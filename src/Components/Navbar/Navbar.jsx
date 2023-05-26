88d53bea430ef6c24d16027fb51cf20a6b515dab
Moizajmal2456 <moizajmal2456@gmail.com>
2023-05-26 11:15:52 +0500

    Add dropdown list in navbar and add products pages in page section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
 import AddCart from "../../Images/Addtocart.png";
 import styles from "./styles.module.scss";
+import { useState } from "react";
 
 export const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    }
 return(
     <div className={styles.Navbar_Wrapper}>
      <div className={styles.Left_Section}>
@@ -10,7 +16,15 @@ return(
             <li className={styles.Home}>
             <Link to="/">Home</Link>
             </li>
-            <li>Products</li>
+            <div className={styles.Select_Wrapper}>
+            <select>
+                <option value="Product">Select Product</option>
+                <option value="Shirt">Shirts</option>
+                <option value="Pents">Pents</option>
+                <option value="Bags">Bags</option>
+                <option value="Shoes">Shoes</option>
+            </select>
+            </div>
             <li>Blogs</li>
             <li className={styles.Home}>
             <Link to="/contactus">Contact Us</Link>
