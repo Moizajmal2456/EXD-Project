5a50ab07796a8862c96d45589a067cef53b2b237
Moizajmal2456 <moizajmal2456@gmail.com>
2023-05-27 18:59:11 +0500

    Add map function to bags cards

--- src/Components/BagsProduct/Bags.jsx
+++ src/Components/BagsProduct/Bags.jsx
@@ -1,33 +1,17 @@
 import style from "./styles.module.scss";
-import Bag1 from "../../Images/BagsPic1.webp";
-import Bag2 from "../../Images/BagsPic2.jpeg";
-import Bag3 from "../../Images/BagsPic3.jpeg";
-import Bag4 from "../../Images/BagsPic4.webp";
 import { Cards } from "../../Cards/Card/Cards";
+import { ProductData } from "../../Data";
 
 export const Bags = () => {
 return(
 <div className={style.Cards_Wrapper}>
+{ProductData.map(product => (
 <Cards 
-image={Bag1}
-heading={"Gents Bag"}
-price={"Rs 1550/-"}
-buttonText={"Buy"}/>
-<Cards 
-image={Bag2}
-heading={"Gents Bag"}
-price={"Rs 1300/-"}
-buttonText={"Buy"}/>
-<Cards 
-image={Bag3}
-heading={"Gents Bag"}
-price={"Rs 1150/-"}
-buttonText={"Buy"}/>
-<Cards 
-image={Bag4}
-heading={"Ladies Bag"}
-price={"Rs 1850/-"}
-buttonText={"Buy"}/>
+image={product.img}
+heading={product.heading}
+price={product.price}
+buttonText={product.buttonText}/>
+))}
 </div>
 );
 };
---
+++ src/Data/index.js
@@ -0,0 +1,27 @@
+export const ProductData = [
+    {
+      img:"../../Images/BagsPic1.webp",
+      heading:"Gents Bag",
+      price:"Rs 1550/-",
+      buttonText:"Buy",
+    },
+    {
+        img:"../../Images/BagsPic1.webp",
+        heading:"Gents Bag",
+        price:"Rs 1550/-",
+        buttonText:"Buy",
+    },
+    { 
+       img :"../../Images/BagsPic1.webp",
+        heading:"Gents Bag",
+        price:"Rs 1550/-",
+        buttonText:"Buy",
+
+    },
+    {
+        img:"../../Images/BagsPic1.webp",
+        heading:"Gents Bag",
+        price:"Rs 1550/-",
+        buttonText:"Buy",
+    },
+];
