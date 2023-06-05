ab4217fe5c82919819cf9c6c233cfe636949e208
Moizajmal2456 <moizajmal2456@gmail.com>
2023-06-05 09:21:27 +0500

    Make changes in signup logic

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import style from "./styles.module.scss";
-import { useNavigate } from "react-router-dom";
+import { redirect, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export const SignUp = () => {
@@ -14,40 +14,49 @@ export const SignUp = () => {
 
   const handleNameChange = ( event) => {
     setUserName(event.target.value);
+    // console.log(setUserName);
+    console.log(userName);
   };
 
   const handleEmailChange = ( event) => {
     setEmail(event.target.value);
+    console.log(email);
   };
 
   const handleMobileChange = ( event) => {
     setMobile(event.target.value);
+    console.log(mobileNo);
   };
  
   const handlePasswordChange = ( event ) => {
    setPassword(event.target.value)
+   console.log(password);
   };
 
-  const signUp = async () => {
-    try {
-      const response = await axios.post('http://localhost:4000/signup', {
-        userName , email , mobileNo , password
-      });
+   const handleSubmit = async () => {
+   try{
+   const response = await axios.post(`http://localhost:4000/signup`, {
+        userName,
+        email,
+        mobileNo,
+        password,
+      })
       console.log(response);
-    } catch (error) {
-      console.error(error);
+   }
+   catch(error){
+    console.log(error)
+   }  
     }
-  };
 
 return(
 <div className={style.SignIn_Wrapper}>
 <div className={style.Input_Fields}>
 <h2>Signup to your account</h2>
-<input className={style.Email} type="string" placeholder="UserName" onClick={handleNameChange}/>
-<input className={style.Email} type="string" placeholder="Email" onClick={handleEmailChange}/>
-<input className={style.Email} type="Number" placeholder="Mobile Number" onClick={handleMobileChange}/>
-<input className={style.Email} type="password" placeholder="Password" onClick={handlePasswordChange}/>
-<button className={style.Button} onClick={signUp}>Sign Up</button>
+<input className={style.Email} type="string" name="userName" placeholder="UserName" onChange={handleNameChange}/>
+<input className={style.Email} type="string" name="email" placeholder="Email" onChange={handleEmailChange}/>
+<input className={style.Email} type="Number" name="mobileNo" placeholder="Mobile Number" onChange={handleMobileChange}/>
+<input className={style.Email} type="password" name="password" placeholder="Password" onChange={handlePasswordChange}/>
+<button className={style.Button} type="submit" onClick={handleSubmit}>Sign Up</button>
 </div>
 </div>
 );
