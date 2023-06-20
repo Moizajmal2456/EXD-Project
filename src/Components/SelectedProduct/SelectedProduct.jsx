68a7eb10d300f68ea34dbb6ee849513fe9755153
Moizajmal2456 <moizajmal2456@gmail.com>
2023-06-20 23:06:16 +0500

    Complete selected product component

--- src/Components/SelectedProduct/SelectedProduct.jsx
+++ src/Components/SelectedProduct/SelectedProduct.jsx
@@ -6,7 +6,6 @@ import { useParams } from "react-router-dom";
 import { FilterButton } from "../Filtering/FilterButton";
 
 export const SelectedProduct = () => {
-
   const allProductData = [...AllProductData];
   // const filtersData = [...filterData];
   const { productType } = useParams();
@@ -15,39 +14,34 @@ export const SelectedProduct = () => {
 
   const filtersData = () => {
   if(productType === "Shoe"){
-  //   return(<FilterButton brands={ShoesBrands}
-  //  variants={ShoesVariant}
-  //  pricerange={Price}/>)
-  console.log(ShoesBrands);
+    return(<div><FilterButton brands={ShoesBrands}
+     variants={ShoesVariant}
+     pricerange={Price}/></div>)
   }
   else if(productType === "Pent"){
-    // return(<FilterButton brands={PentsBrands}
-    // variants={PentsVariant}
-    // pricerange={Price}/>)
-    // console.log(PentsBrands);
+    return(<div><FilterButton brands={PentsBrands}
+    variants={PentsVariant}
+    pricerange={Price}/></div>)
    }
    else if(productType === "Bag"){
-    // return(<FilterButton brands={BagsBrands}
-    // variants={BagsVariant}
-    // pricerange={Price}/>) 
-    // console.log(BagsBrands);
+    return(<FilterButton brands={BagsBrands}
+    variants={BagsVariant}
+    pricerange={Price}/>) 
    }
    else {
-    // return(<FilterButton brands={ShirtsBrands}
-    // variants={ShirtsVariant}
-    // pricerange={Price}/>)
-    // console.log(ShoesBrands);
+    return(<div><FilterButton brands={ShirtsBrands}
+    variants={ShirtsVariant}
+    pricerange={Price}/></div>)
    }
   };
     return (
     <div className={style.Filters_Warpper}>
       <div className={style.Filters}>
-      
+      {filtersData()}
       </div>
       <div className={style.Products_Wrapper}>
         {productData.map((obj) => {
           return(
-          filtersData(),
           <Cards
             image={obj.img}
             heading={obj.heading}
