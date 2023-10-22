e06e209aaefa854c821402f9320e4d40fd76d205
Moizajmal2456 <moizajmal2456@gmail.com>
2023-10-22 14:51:36 -0400

    Changes in Add to cart component, data and filtering component

--- src/Components/Filtering/FilterButton.jsx
+++ src/Components/Filtering/FilterButton.jsx
@@ -1,15 +1,6 @@
 import style from "./styles.module.scss";
 
 export const FilterButton = ({brands, variants, pricerange }) => {
-
-const BrandsFilter =  ( event ) => {
-    // const { value } = event.target;
-    // if(value === 0) 
-    // {
-    // return ;
-    // }
-    // const BrandFilteredProduct = data.filter((m) => m.brand === value);
-};
 return(
     <div className={style.Fiters_Wrapper} >
         <div className={style.BrandFilter}>
