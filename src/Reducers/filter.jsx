f025477278925c6147377924c8ef36a2a1edf408
Moizajmal2456 <moizajmal2456@gmail.com>
2023-11-09 16:09:09 -0500

    Changes in filter reducer

--- src/Reducers/filter.jsx
+++ src/Reducers/filter.jsx
@@ -1,12 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  brands: "", // Selected value for the brands select element
+  variants: "", // Selected value for the variants select element
+  price: "", // Selected value for the price select element
+};
+
 const filterSlice = createSlice ({
  name:"filter",
- initialState: "",
+ initialState,
    reducers:{
-     brandsFilter: (state) => state,
-     variantsFilter: (state) => state,
-     priceFilter: (state) => state,
+     brandsFilter: (state, action) => state.brands = action.payload,
+     variantsFilter: (state, action) => state.variants = action.payload,
+     priceFilter: (state, action) => state.price = action.payload,
     },
 });
 
